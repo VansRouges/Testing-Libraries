a09f92c45344504dc4ef5a615c6c6b6c6f21ee04
VansRouges <evansagina57@gmail.com>
2023-08-24 15:52:51 +0100

    vanilla testing

---
+++ test vanilla js/index.js
@@ -0,0 +1,34 @@
+// // index.js
+function isPalindrome(string) {
+    // O(n)
+    // Put a pointer at each extreme of the word and iterate "inwards"
+    // At each iteration, check if the pointers represent equal values
+    // If this condition isn't accomplished, the word isn't a palindrome
+    let left = 0
+    let right = string.length-1
+  
+    while (left < right) {
+        if (string[left] === string[right]) {
+            left += 1
+            right -= 1
+        }
+        else return false
+    }
+  
+    return true
+}
+
+// index.js
+function twoSum(nums, target) {
+    // O(n)
+    // Iterate the array once
+    // At each iteration, calculate the value needed to get to the target, which is target - currentValue
+    // If the neededValue exists in the array, return [currentValue, neededValue], else continue iteration
+	for (let i = 0; i < nums.length; i++) {
+		const neededNum = target - nums[i]
+		if (nums.indexOf(neededNum) !== -1 && nums.indexOf(neededNum) !== i) return [nums[i], nums[nums.indexOf(neededNum)]]
+	}
+    return false
+}
+
+module.exports = { isPalindrome, twoSum }
---
+++ test vanilla js/index.test.js
@@ -0,0 +1,26 @@
+// index.test.js
+({ isPalindrome, twoSum } = require('./index.js'))
+
+describe('isPalindrome function', () => {
+  test('neuquen is palindrom', () => {
+    expect(isPalindrome("neuquen")).toBe(true)
+  })
+
+  test('bariloche is not palindrom', () => {
+    expect(isPalindrome("bariloche")).toBe(false)
+  })
+})
+
+describe('twoSum function', () => {
+  test('[2,7,11,15] and 9 returns [2, 7]', () => {
+    expect(twoSum([2,7,11,15], 9)).toEqual([2,7])
+  })
+
+  test('[3,2,4] and 6 returns [2, 4]', () => {
+    expect(twoSum([3,2,4], 6)).toEqual([2,4])
+  })
+
+  test('[3,2,4] and 10 returns false', () => {
+    expect(twoSum([3,2,4], 10)).toBe(false)
+  })
+})
